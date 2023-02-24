e29445885b0e0e932d03c7da02c468265b678a38
fedorzvyk <fedorzvyk@gmail.com>
2023-02-24 11:16:46 +0200

    edited path

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           {/* <ChakraProvider theme={theme}> */}
-          <BrowserRouter basename="/phonebook-">
+          <BrowserRouter basename="/phonebook">
             <App />
           </BrowserRouter>
           {/* </ChakraProvider> */}
